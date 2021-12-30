77a4cdab5805cddecc65559bd609fb858c41f523
doyiblue <67153423+doyoung926@users.noreply.github.com>
2021-12-30 18:29:04 +0900

    v8.0

--- loop.c
+++ loop.c
@@ -13,11 +13,21 @@ int main(void)
     printf("Hello World %d\n", i++);
   }*/
 
-  int i = 1;
+  /*int i = 1;
   do
   {
     printf("Hello World %d\n", i++);
-  } while (i<=10);
+  } while (i<=10);*/
+
+  for (int i=1; i<=3; i++)
+  {
+    printf("첫 번째 반복문 : %d\n", i);
+
+    for (int j=1; j<=5; j++)
+    {
+      printf("두 번째 반복문 : %d\n", j);
+    }
+  }
 
   return 0;
 }

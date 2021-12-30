2e69ac51ad020184f76619b2e37f1d1ce0c7aaee
doyiblue <67153423+doyoung926@users.noreply.github.com>
2021-12-30 18:37:51 +0900

    v10.1

--- loop.c
+++ loop.c
@@ -38,14 +38,14 @@ int main(void)
     }
   }*/
 
-  for (int i=0; i<5; i++)
+  /*for (int i=0; i<5; i++)
   {
     for (int j=0; j<=i; j++)
     {
       printf("*");
     }
     printf("\n");
-  }
+  }*/
 
   return 0;
 }

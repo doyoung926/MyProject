0e367169584b480e3b62b12e3eaea47ccd6a55e3
doyiblue <67153423+doyoung926@users.noreply.github.com>
2022-01-02 16:02:15 +0900

    v11.0

---
+++ data.c
@@ -0,0 +1,19 @@
+//variable - 변할 수 있는 어떤 것
+//constant - 변할 수 없는 어떤 것
+
+/*
+홍길동
+  - 나이 (변수)
+  - 생일 (상수)
+*/
+
+#include <stdio.h>
+#define pi 3.1415 // constant
+
+int main(void)
+{
+  // variable
+  int age = 14;
+  float weight;
+  char letter_grade = 'a';
+}
